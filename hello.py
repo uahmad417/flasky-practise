92a0e05a8be07d6a0728005e794dfd7350d45a58
uahmad417 <18u.ahmad@gmail.com>
2023-07-28 15:28:31 +0500

    fixed formating

--- hello.py
+++ hello.py
@@ -17,34 +17,22 @@ bootstrap = Bootstrap(app)
 # root URL
 @app.route('/')
 def index():
-    response = make_response(
-        render_template('index.html')
-        )
+    response = make_response(render_template('index.html'))
     return response
 
 # user URL
 @app.route('/user/<name>')
 def user(name):
-    response = make_response(
-        render_template('user.html',name=name)
-        )
+    response = make_response(render_template('user.html',name=name))
     return response
 
 # error handling views
 @app.errorhandler(404)
 def page_not_found(e):
-    response = make_response(
-        render_template('404.html'),
-        404
-    )
-
+    response = make_response(render_template('404.html'),404)
     return response
 
 @app.errorhandler(500)
 def internal_error(e):
-    response = make_response(
-        render_template('500.html'),
-        500
-    )
-
+    response = make_response(render_template('500.html'),500)
     return response
